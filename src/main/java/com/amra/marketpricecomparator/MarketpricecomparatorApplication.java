ae986f7a3af9201eeaad4ff72890194ad56e74e8
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-11-06 22:22:36 +0100

    Se crean los archivos

---
+++ src/main/java/com/amra/marketpricecomparator/MarketpricecomparatorApplication.java
@@ -0,0 +1,13 @@
+package com.amra.marketpricecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketpricecomparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketpricecomparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amra/marketpricecomparator/MarketpricecomparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amra.marketpricecomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketpricecomparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

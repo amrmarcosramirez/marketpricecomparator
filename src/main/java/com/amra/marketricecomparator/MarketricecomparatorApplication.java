cd988177c5e3f8b2dc9181a553a17d5d8d816370
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-11-06 21:45:27 +0100

    se crear los archivos

---
+++ src/main/java/com/amra/marketricecomparator/MarketricecomparatorApplication.java
@@ -0,0 +1,13 @@
+package com.amra.marketricecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketricecomparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketricecomparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amra/marketricecomparator/MarketricecomparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amra.marketricecomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketricecomparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
